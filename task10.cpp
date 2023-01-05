d09c3b90deb92a97d45d80f7dea4dcf82d8272c0
AbuzarAslam <120781089+AbuzarAslam@users.noreply.github.com>
2023-01-05 07:51:36 -0800

    Add files via upload

--- task10.cpp
+++ task10.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 main () {
 int n;
-cout "Enter 5 number..." ;
-cin << n;
-cin << n;
-cin << n;
-cin << n;
-cin << n;
+int sum = 0;
+cout << "Enter 1st number..." ;
+cin >> n;
+cout << "Enter 2nd number..." ;
+sum = sum + n ;
+cin >> n;
+cout << "Enter 3rd number..." ;
+sum = sum + n ; 
+cin >> n;
+cout << "Enter 4th number..." ;
+sum = sum + n ; 
+cin >> n;
+cout << "Enter 5th number..." ;
+sum = sum + n ; 
+cin >> n;
+cout << "sum is....." << sum;
 }
